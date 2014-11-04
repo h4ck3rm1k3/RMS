95f5c08cfc76d60393bf171b9a773247cee6c848
James Michael DuPont <JamesMikeDuPont@gmail.com>
2014-11-04 06:14:46 -0600

    segfault

---
+++ import.sh
@@ -0,0 +1,2 @@
+which osm2pgsql
+osm2pgsql --create --cache 100 --slim -S /opt/RMS/RMS.style -H db -U docker  -d routing  /var/lib/splitter/australia-oceania/australia/queensland/brisbane/example.osm

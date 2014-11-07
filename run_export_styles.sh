f7029b6ed95e9b1fbf059e09cd4c9557d93f6135
James Michael DuPont <JamesMikeDuPont@gmail.com>
2014-11-07 07:41:38 -0600

    now export png is working, next to tweak the styles

--- run_export_styles.sh
+++ run_export_styles.sh
@@ -1,3 +1,4 @@
 cd /opt/tilemill
 #node ./index.js export --format mbtiles RMS /opt/RMS/export/ --bbox=153.020325,-27.568182,152.850037,-27.686811
-node ./index.js export --format mbtiles RMS /opt/RMS/export/ --bbox=-27.568182,153.020325,-27.686811,152.850037
+#node ./index.js export --format mbtiles RMS /opt/RMS/export/ --bbox=-27.568182,153.020325,-27.686811,152.850037
+node ./index.js export --format png --bbox="152.918,-27.6397,152.9609,-27.6133" RMS /opt/RMS/export2.png
